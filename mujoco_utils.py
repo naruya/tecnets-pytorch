9b6aa9403482e7ee80a048265f925d32739d02e3
Naruya Kondo <n-kondo@weblab.t.u-tokyo.ac.jp>
2019-06-05 03:02:51 +0900

    clean

--- mujoco_utils.py
+++
@@ -1,6 +0,0 @@
-from gym.envs.mujoco.pusher import PusherEnv
-
-def load_env(xml_path):
-    xml_filepath = 'sim_push_xmls/' + xml_path.split("/")[-1]
-    env = PusherEnv(**{'xml_file':xml_filepath, 'distractors': True})
-    return env
