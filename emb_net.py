16b9fe6dc068e931e556c20e6124362daa0f2fdb
Naruya Kondo <n-kondo@weblab.t.u-tokyo.ac.jp>
2019-06-14 10:54:38 +0000

    add mini64

--- emb_net.py
+++ emb_net.py
@@ -8,38 +8,38 @@ class EmbeddingNet(nn.Module):
         
         self.h = h = 16
         
-        self.conv1 = nn.Conv2d(3*2, h, kernel_size=5, stride=2)
-        self.conv2 = nn.Conv2d(h, h, kernel_size=5, stride=2)
-        self.conv3 = nn.Conv2d(h, h, kernel_size=5, stride=2)
-        self.conv4 = nn.Conv2d(h, h, kernel_size=5, stride=2)
+        self.conv1 = nn.Conv2d(3*2, h, kernel_size=3, stride=1)
+        self.conv2 = nn.Conv2d(h, h, kernel_size=3, stride=1)
+        self.conv3 = nn.Conv2d(h, h, kernel_size=3, stride=1)
+        self.conv4 = nn.Conv2d(h, h, kernel_size=3, stride=1)
         
-        self.ln1 = nn.LayerNorm([h, 61, 61])
-        self.ln2 = nn.LayerNorm([h, 29, 29])
-        self.ln3 = nn.LayerNorm([h, 13, 13])
-        self.ln4 = nn.LayerNorm([h, 5, 5])
+        self.ln1 = nn.LayerNorm([h, 32, 32])
+        self.ln2 = nn.LayerNorm([h, 16, 16])
+        self.ln3 = nn.LayerNorm([h, 8, 8])
+        self.ln4 = nn.LayerNorm([h, 4, 4])
         
-        self.fc1 = nn.Linear(h*5*5, 200)
-        self.fc2 = nn.Linear(200, 200)
-        self.fc3 = nn.Linear(200, 20)
+        self.fc1 = nn.Linear(h*4*4, 128)
+        self.fc2 = nn.Linear(128, 128)
+        self.fc3 = nn.Linear(128, 20)
         
-        self.ln5 = nn.LayerNorm([200])
-        self.ln6 = nn.LayerNorm([200])
+        self.ln5 = nn.LayerNorm([128])
+        self.ln6 = nn.LayerNorm([128])
         
         self._init_weights()
 
     def forward(self, vision):
         
-        x = vision # 6x125x125
+        x = vision # 6x64x64
 
-        x = F.elu(self.ln1(self.conv1(x))) # hx61x61
-        x = F.elu(self.ln2(self.conv2(x))) # hx29x29
-        x = F.elu(self.ln3(self.conv3(x))) # hx13x13
-        x = F.elu(self.ln4(self.conv4(x))) # hx5x5
+        x = F.elu(self.ln1(self.conv1(x))) # hx32x32
+        x = F.elu(self.ln2(self.conv2(x))) # hx16x16
+        x = F.elu(self.ln3(self.conv3(x))) # hx8x8
+        x = F.elu(self.ln4(self.conv4(x))) # hx4x4
         
-        x = x.view(x.shape[0], self.h*5*5) # h*5*5
+        x = x.view(x.shape[0], self.h*4*4) # h*4*4
         
-        x = F.elu(self.ln5(self.fc1(x))) # 200
-        x = F.elu(self.ln6(self.fc2(x))) # 200
+        x = F.elu(self.ln5(self.fc1(x))) # 128
+        x = F.elu(self.ln6(self.fc2(x))) # 128
         x = self.fc3(x) # 20
 
         return x
@@ -53,6 +53,6 @@ class EmbeddingNet(nn.Module):
                 init.normal_(m.weight, 0, 0.01)
                 init.constant_(m.bias, 0)
             elif isinstance(m, nn.LayerNorm):
-                # init.constant_(m.weight, 1)
-                m.register_parameter('weight', None)
+                init.constant_(m.weight, 1)
+                # m.register_parameter('weight', None)
                 init.constant_(m.bias, 0)
