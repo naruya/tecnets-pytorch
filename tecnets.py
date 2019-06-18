ec960db3ae8b88c77e8a1083ba88a86c665d0d88
Naruya Kondo <n-kondo@weblab.t.u-tokyo.ac.jp>
2019-06-18 15:12:43 +0000

    not lambda_emb=0

--- tecnets.py
+++ tecnets.py
@@ -92,22 +92,21 @@ class TecNets(MetaLearner):
                 q_sj_list, U_sj_list, U_si_list = [], [], []
 
                 # ---- calc loss_emb ----
-                """
                 for jdx, (q_sj, U_sj) in enumerate(zip(q_s_list, U_s_list)):
                     for idx, U_si in enumerate(U_s_list):
                         if jdx == idx: continue
                         q_sj_list.append(q_sj)
                         U_sj_list.append(U_sj)
                         U_si_list.append(U_si)
-                """
-                loss_emb = 0 # torch.sum(self.cos_hinge_loss(q_sj_list, U_sj_list, U_si_list)) * 1.0
+
+                loss_emb = torch.sum(self.cos_hinge_loss(q_sj_list, U_sj_list, U_si_list)) * 1.0
 
                 loss = loss_emb + loss_ctr_U + loss_ctr_q
                 if train:
                     loss.backward()
 
                 loss = loss.item()
-                # loss_emb = loss_emb.item()
+                loss_emb = loss_emb.item()
                 loss_ctr_q = loss_ctr_q.item()
                 loss_ctr_U = loss_ctr_U.item()
                 # ----
