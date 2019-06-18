44228b7198df136430548d7512f720a32ac5f550
Naruya Kondo <n-kondo@weblab.t.u-tokyo.ac.jp>
2019-06-18 17:11:43 +0000

    load N tasks

--- tecnets.py
+++ tecnets.py
@@ -14,60 +14,68 @@ class TecNets(MetaLearner):
     def __init__(self, device, log_dir=None, lr=None):
         super(TecNets, self).__init__(device, log_dir, lr)
 
-    def make_sentence(self, vision, normalize):             # k,100,3,125,125
-        inp = torch.cat([vision[:,0], vision[:,-1]], dim=1) # k,6,125,125
-        sj = self.emb_net(inp.to(self.device))              # k,20
+    def make_sentence(self, vision, normalize):
+        N, k, _F, _C, H, W = vision.shape                       # N,k,100,3,H,W
+        inp = torch.cat([vision[:,:,0], vision[:,:,-1]], dim=2) # N,k,6,H,W
+        sj = self.emb_net(inp.view(N*k,6,H,W)).view(N,k,20)     # N,k,20
         if normalize:
-            sj = sj.mean(0) # 20,
-            sj = sj / torch.norm(sj)
+            sj = sj.mean(1) # N,20
+            sj = sj / torch.norm(sj, 1)
         else:
-            sj = sj[0]      # 20,
+            sj = sj[:,0]    # N,20
         return sj
 
-    def cos_hinge_loss(self, q_sj, U_sj, U_si):
-        real = torch.dot(q_sj, U_sj)
-        fake = torch.dot(q_sj, U_si)
-        zero = torch.zeros(1).to(self.device)
-        return torch.max(zero, 0.1 - real + fake)
+    def cos_hinge_loss(self, q_sj_list, U_sj_list, U_si_list):
+        q_sj = torch.stack(q_sj_list) # 4032, 20
+        U_sj = torch.stack(U_sj_list) # 4032, 20
+        U_si = torch.stack(U_si_list) # 4032, 20
 
-    def meta_train(self, task_loader, num_batch_tasks, epoch, writer=None, train=True):
+        real = (q_sj*U_sj).sum(1) # 4032,
+        fake = (q_sj*U_si).sum(1) # 4032,
+        zero = torch.zeros(1).to(self.device) # 1,
+
+        loss = torch.max(0.1 - real + fake, zero) # 4032,
+        return loss
+
+    def meta_train(self, task_loader, num_batch_tasks, num_load_tasks, epoch, writer=None, train=True):
         device = self.device
 
         B = num_batch_tasks
+        N = num_load_tasks  # e.g. 16
         loss_emb_list, loss_ctr_U_list, loss_ctr_q_list, loss_list = [], [], [], []
 
-        for i, task in enumerate(tqdm(task_loader)): # load tasks one by one
+        # N tasks * (n_tasks/N)iter # e.g. 16task * 44iter
+        for i, tasks in enumerate(tqdm(task_loader)):
 
-            if i % B == 0:
+            if (i*N) % B == 0:
                 if train:
                     self.opt.zero_grad()
                 loss_emb, loss_ctr_U, loss_ctr_q = 0, 0, 0
-                U_s_dict, q_s_dict = {}, {}
+                U_s_list, q_s_list = [], []
 
-            U_vision = task["train-vision"][0] # U_n,100,3,125,125
-            U_state = task["train-state"][0]   # U_n,100,20
-            U_action = task["train-action"][0] # U_n,100,7
-            q_vision = task["test-vision"][0]  # q_n,100,3,125,125
-            q_state = task["test-state"][0]    # q_n,100,20
-            q_action = task["test-action"][0]  # q_n,100,7
-            jdx = task["idx"].item()           # 1
-            U_n, q_n = len(U_vision), len(q_vision)
-            size = U_vision.shape[-1] # 125 or 64
+            U_vision = tasks["train-vision"] # N,U_n,100,3,125,125
+            U_state = tasks["train-state"]   # N,U_n,100,20
+            U_action = tasks["train-action"] # N,U_n,100,7
+            q_vision = tasks["test-vision"]  # N,q_n,100,3,125,125
+            q_state = tasks["test-state"]    # N,q_n,100,20
+            q_action = tasks["test-action"]  # N,q_n,100,7
+            U_n, q_n = U_vision.shape[1], q_vision.shape[1]
+            size = U_vision.shape[5] # 125 or 64
 
-            U_sj = self.make_sentence(U_vision, True)
-            q_sj = self.make_sentence(q_vision, False)
-            U_s_dict[jdx] = U_sj
-            q_s_dict[jdx] = q_sj
+            U_sj = self.make_sentence(U_vision, normalize=True)  # N,20
+            q_sj = self.make_sentence(q_vision, normalize=False) # N,20
+            U_s_list.append(U_sj)
+            q_s_list.append(q_sj)
 
             # ---- calc loss_ctr ----
-            U_vision = U_vision.view(U_n*100,3,size,size).to(device)
-            U_state = U_state.view(U_n*100,20).to(device)
-            U_action = U_action.view(U_n*100,7).to(device)
-            q_vision = q_vision.view(q_n*100,3,size,size).to(device)
-            q_state = q_state.view(q_n*100,20).to(device)
-            q_action = q_action.view(q_n*100,7).to(device)
-            U_sj_U_inp = U_sj.repeat_interleave(100*U_n, dim=0)
-            U_sj_q_inp = U_sj.repeat_interleave(100*q_n, dim=0)
+            U_vision = U_vision.view(N*U_n*100,3,size,size).to(device) # N*U_n*100,C,H,W
+            U_state = U_state.view(N*U_n*100,20).to(device)            # N*U_n*100,20
+            U_action = U_action.view(N*U_n*100,7).to(device)           # N*U_n*100,7
+            q_vision = q_vision.view(N*q_n*100,3,size,size).to(device) # N*q_n*100,C,H,W
+            q_state = q_state.view(N*q_n*100,20).to(device)            # N*q_n*100,20
+            q_action = q_action.view(N*q_n*100,7).to(device)           # N*q_n*100,7
+            U_sj_U_inp = U_sj.repeat_interleave(100*U_n, dim=0)        # N*U_n*100,20
+            U_sj_q_inp = U_sj.repeat_interleave(100*q_n, dim=0)        # N*q_n*100,20
 
             U_out = self.ctr_net(U_vision, U_sj_U_inp, U_state)
             _loss_ctr_U = self.loss_fn(U_out, U_action) * len(U_vision) * 0.1
@@ -82,16 +90,27 @@ class TecNets(MetaLearner):
             loss_ctr_q += _loss_ctr_q.item()
             # ----
 
-            if (i+1) % B == 0:
+            if ((i+1)*N) % B == 0:
+
+                # don't convert into list. graph informations will be lost. (and an error will occur)
+                U_s_list = torch.cat(U_s_list, 0) # N*(B/N),20 -> N,20
+                q_s_list = torch.cat(q_s_list, 0) # N*(B/N),20 -> N,20
+
+                q_sj_list, U_sj_list, U_si_list = [], [], []
 
                 # ---- calc loss_emb ----
-                for (jdx, q_sj), (_, U_sj) in zip(q_s_dict.items(), U_s_dict.items()):
-                    for (idx, U_si) in U_s_dict.items():
+                for jdx, (q_sj, U_sj) in enumerate(zip(q_s_list, U_s_list)):
+                    for idx, U_si in enumerate(U_s_list):
                         if jdx == idx: continue
-                        loss_emb += self.cos_hinge_loss(q_sj, U_sj, U_si) * 1.0
+                        q_sj_list.append(q_sj)
+                        U_sj_list.append(U_sj)
+                        U_si_list.append(U_si)
+
+                _loss_emb = torch.sum(self.cos_hinge_loss(q_sj_list, U_sj_list, U_si_list)) * 1.0
+
                 if train:
-                    loss_emb.backward()
-                loss_emb = loss_emb.item()
+                    _loss_emb.backward()
+                loss_emb = _loss_emb.item()
                 # ----
 
                 if train:
