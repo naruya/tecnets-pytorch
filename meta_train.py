31185bc297b95d5633a627fcb036412721a0f9d7
Naruya Kondo <n-kondo@weblab.t.u-tokyo.ac.jp>
2019-06-27 17:11:48 +0000

    add log_dir option

--- meta_train.py
+++ meta_train.py
@@ -17,6 +17,7 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Task-Embedded Control Networks Implementation')
     parser.add_argument('--device_ids', type=int, nargs='+', help='list of CUDA devices (default: [0])', default=[0])
     parser.add_argument('--demo_dir', type=str, default='../mil/data/sim_push/')
+    parser.add_argument('--log_dir', type=str, default=None)
     parser.add_argument('--num_batch_tasks', type=int, default=64)
     parser.add_argument('--state_path', type=str, default="../mil/data/sim_push_common/scale_and_bias_sim_push.pkl")
     parser.add_argument('--train_n_shot', type=int, default=1)
@@ -32,22 +33,25 @@ if __name__ == '__main__':
     random.seed(args.seed)
     device = "cuda"
 
-    _cmd = "git rev-parse --short HEAD"
-    commit = subprocess.check_output(_cmd.split()).strip().decode('utf-8')
+    if args.log_dir:
+        log_dir = args.log_dir
+        print(log_dir)
+    else:
+        _cmd = "git rev-parse --short HEAD"
+        commit = subprocess.check_output(_cmd.split()).strip().decode('utf-8')
+        _cmd = "git rev-parse --abbrev-ref HEAD"
+        branch = subprocess.check_output(_cmd.split()).strip().decode('utf-8')
+        branch = "-".join(branch.split("/"))
 
-    _cmd = "git rev-parse --abbrev-ref HEAD"
-    branch = subprocess.check_output(_cmd.split()).strip().decode('utf-8')
-    branch = "-".join(branch.split("/"))
+        log_dir = "./logs/" + datetime.now().strftime('%m%d-%H%M%S') \
+                            + "_" + branch \
+                            + "_" + commit \
+                            + "_B"+str(args.num_batch_tasks) \
+                            + "_size125" \
+                            + "_shot" + str(args.train_n_shot) + "-" + str(args.test_n_shot)
 
-    log_dir = "./logs/" + datetime.now().strftime('%m%d-%H%M%S') \
-                        + "_" + branch \
-                        + "_" + commit \
-                        + "_B"+str(args.num_batch_tasks) \
-                        + "_size125" \
-                        + "_shot" + str(args.train_n_shot) + "-" + str(args.test_n_shot)
-
-    print(log_dir)
-    os.mkdir(log_dir)
+        print(log_dir)
+        os.mkdir(log_dir)
 
     train_writer = SummaryWriter("../../runs/"+log_dir.split("/")[-1]+"_train")
     valid_writer = SummaryWriter("../../runs/"+log_dir.split("/")[-1]+"_valid")
