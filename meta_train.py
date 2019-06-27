57f54f9b7777ea155e5f846ced908c27d671ff8d
Naruya Kondo <n-kondo@weblab.t.u-tokyo.ac.jp>
2019-06-27 07:50:16 +0000

    add test_n_shot arg

--- meta_train.py
+++ meta_train.py
@@ -20,6 +20,7 @@ if __name__ == '__main__':
     parser.add_argument('--num_batch_tasks', type=int, default=64)
     parser.add_argument('--state_path', type=str, default="../mil/data/sim_push_common/scale_and_bias_sim_push.pkl")
     parser.add_argument('--train_n_shot', type=int, default=1)
+    parser.add_argument('--test_n_shot', type=int, default=1)
     parser.add_argument('--seed', type=int, default=123)
     args = parser.parse_args()
 
