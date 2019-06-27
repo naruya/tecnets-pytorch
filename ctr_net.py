4d7d55afcd87da805f45fc23359239ca5267c096
Naruya Kondo <n-kondo@weblab.t.u-tokyo.ac.jp>
2019-06-27 07:43:48 +0000

    fix model architecture

--- ctr_net.py
+++ ctr_net.py
@@ -9,22 +9,19 @@ class ControlNet(nn.Module):
         
         self.h = h = 16
         
-        self.conv1 = nn.Conv2d(3+20, h, kernel_size=5, stride=2)
-        self.conv2 = nn.Conv2d(h, h, kernel_size=5, stride=2)
-        self.conv3 = nn.Conv2d(h, h, kernel_size=5, stride=2)
-        self.conv4 = nn.Conv2d(h, h, kernel_size=5, stride=2)
+        self.conv1 = nn.Conv2d(3+20, h, kernel_size=5, stride=2, padding=2)
+        self.conv2 = nn.Conv2d(h, h, kernel_size=5, stride=2, padding=2)
+        self.conv3 = nn.Conv2d(h, h, kernel_size=5, stride=2, padding=2)
+        self.conv4 = nn.Conv2d(h, h, kernel_size=5, stride=2, padding=2)
         
-        self.ln1 = nn.LayerNorm([h, 61, 61])
-        self.ln2 = nn.LayerNorm([h, 29, 29])
-        self.ln3 = nn.LayerNorm([h, 13, 13])
-        self.ln4 = nn.LayerNorm([h, 5, 5])
+        self.ln1 = nn.GroupNorm(1, h)  # LayerNorm (sharing affine transform parameter in channel ver.)
+        self.ln2 = nn.GroupNorm(1, h)
+        self.ln3 = nn.GroupNorm(1, h)
+        self.ln4 = nn.GroupNorm(1, h)
         
-        self.fc1 = nn.Linear(h*5*5+20, 200)
+        self.fc1 = nn.Linear(h*8*8+20, 200)
         self.fc2 = nn.Linear(200, 200)
         self.fc3 = nn.Linear(200, 7)
-        
-        self.ln5 = nn.LayerNorm([200])
-        self.ln6 = nn.LayerNorm([200])
         
         self._init_weights()
 
@@ -34,17 +31,17 @@ class ControlNet(nn.Module):
 
         x = torch.cat((vision, sentence), 1) # 23x125x125
     
-        x = F.elu(self.ln1(self.conv1(x))) # hx61x61
-        x = F.elu(self.ln2(self.conv2(x))) # hx29x29
-        x = F.elu(self.ln3(self.conv3(x))) # hx13x13
-        x = F.elu(self.ln4(self.conv4(x))) # hx5x5
+        x = F.elu(self.ln1(self.conv1(x))) # hx63x63
+        x = F.elu(self.ln2(self.conv2(x))) # hx32x32
+        x = F.elu(self.ln3(self.conv3(x))) # hx16x16
+        x = F.elu(self.ln4(self.conv4(x))) # hx8x8
         
-        x = x.view(x.shape[0], self.h*5*5) # h*5*5
-        x = torch.cat((x, state), 1)  # h*5*5+20
+        x = x.view(x.shape[0], self.h*8*8) # h*8*8
+        x = torch.cat((x, state), 1)  # h*8*8+20
 
-        x = F.elu(self.ln5(self.fc1(x))) # 200
-        x = F.elu(self.ln6(self.fc2(x))) # 200
-        x = self.fc3(x)*2 # 7
+        x = F.elu(self.fc1(x)) # 200
+        x = F.elu(self.fc2(x)) # 200
+        x = self.fc3(x) # 7
 
         return x
     
@@ -56,7 +53,7 @@ class ControlNet(nn.Module):
             elif isinstance(m, nn.Linear):
                 init.normal_(m.weight, 0, 0.01)
                 init.constant_(m.bias, 0)
-            elif isinstance(m, nn.LayerNorm):
-                # init.constant_(m.weight, 1)
-                m.register_parameter('weight', None)
+            elif isinstance(m, nn.GroupNorm):
+                init.constant_(m.weight, 1)
+                # m.register_parameter('weight', None)
                 init.constant_(m.bias, 0)
