3666644035068a7c57a0023847accf5523f21fd3
Naruya Kondo <n-kondo@weblab.t.u-tokyo.ac.jp>
2019-06-30 10:47:55 +0000

    fix run.sh (to data_mini)

--- run.sh
+++ run.sh
@@ -2,4 +2,4 @@
 # script -c ./run.sh /dev/null | tee output.log
 # export CUDA_VISIBLE_DEVICES=0,1,2,3
 # export OMP_NUM_THREADS=16
-python meta_train.py --demo_dir="../../data/mil/data/sim_push/" --state_path="../../data/mil/data/scale_and_bias_sim_push.pkl"
+python meta_train.py --demo_dir="../../data/mil/data_mini/sim_push/" --state_path="../../data/mil/data/scale_and_bias_sim_push.pkl"
