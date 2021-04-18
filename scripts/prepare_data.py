451889e4344c8a841fa09858603fdc23debf4f06
shogi880 <shogi-880@g.ecc.u-tokyo.ac.jp>
2021-04-18 23:32:35 +0900

    fix

--- scripts/prepare_data.py
+++ scripts/prepare_data.py
@@ -7,6 +7,7 @@ task_info_paths = glob.glob(task_paths)
 num_support = 5
 num_query = 1
 
+print(task_info_paths)
 for index in range(3):
     pickle_file = task_info_paths[index]
     with open(pickle_file, 'rb') as f:
