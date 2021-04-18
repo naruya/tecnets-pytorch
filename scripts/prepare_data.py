46ac68ad919847444d43a5bdfcde36db6b2ecb77
shogi880 <shogi-880@g.ecc.u-tokyo.ac.jp>
2021-04-18 23:31:05 +0900

    prepare_data

---
+++ scripts/prepare_data.py
@@ -0,0 +1,13 @@
+import glob
+
+demo_dir='./datasets/mil_sim_push/'
+task_paths = f'{demo_dir}train/task_*.pkl'
+task_info_paths = glob.glob(task_paths)
+
+num_support=5, num_query=1
+
+for index in range(3):
+    pickle_file = task_info_paths[index]
+    with open(pickle_file, 'rb') as f:
+        data = pickle.load(f)
+    print(data)
