48122260589be83f3c229ed1820f52e4f7aaa917
shogi880 <shogi-880@g.ecc.u-tokyo.ac.jp>
2021-04-18 21:14:59 +0900

    fix

--- tecnets.py
+++ tecnets.py
@@ -10,43 +10,57 @@ from utils import vread
 
 import time
 
+
 class TecNets(MetaLearner):
     def __init__(self, device, lr=None):
         super(TecNets, self).__init__(device, lr)
 
     def make_sentence(self, image, normalize):
-        N, k, _F, _C,  W, H = image.shape
-        # print('image_shape : ',image.shape)                    # N,k,100,3,H,W
-        inp = torch.cat([image[:,:,0], image[:,:,-1]], dim=2) # N,k,6,H,W
+        N, k, _F, _C, W, H = image.shape
+        # print('image_shape : ',image.shape)                    #
+        # N,k,100,3,H,W
+        inp = torch.cat([image[:, :, 0], image[:, :, -1]], dim=2)  # N,k,6,H,W
         # print(inp.shape)
-        inp = inp.view(N * k,6,H,W)
+        inp = inp.view(N * k, 6, H, W)
         # print(inp.shape)
 
         # import ipdb; ipdb.set_trace()
-        sentence = self.emb_net(inp.view(N * k,6,H,W)).view(N,k,20)     # N,k,20
+        sentence = self.emb_net(
+            inp.view(N * k, 6, H, W)).view(N, k, 20)     # N,k,20
         # print(sentence)
         if normalize:
-            sentence = sentence.mean(1) # N,20
+            sentence = sentence.mean(1)  # N,20
             sentence = sentence / torch.norm(sentence, 1)
         else:
-            sentence = sentence[:,0]    # N,20
+            sentence = sentence[:, 0]    # N,20
 
         return sentence
 
-    def cos_hinge_loss(self, query_sentence_list, support_sentence_list, U_si_list):
+    def cos_hinge_loss(
+            self,
+            query_sentence_list,
+            support_sentence_list,
+            U_si_list):
         print(len(query_sentence_list))
-        query_sentence = torch.stack(query_sentence_list) # 4032, 20
-        support_sentence = torch.stack(support_sentence_list) # 4032, 20
-        U_si = torch.stack(U_si_list) # 4032, 20
+        query_sentence = torch.stack(query_sentence_list)  # 4032, 20
+        support_sentence = torch.stack(support_sentence_list)  # 4032, 20
+        U_si = torch.stack(U_si_list)  # 4032, 20
 
-        real = (query_sentence * support_sentence).sum(1) # 4032,
-        fake = (query_sentence * U_si).sum(1) # 4032,
-        zero = torch.zeros(1).to(self.device) # 1,
+        real = (query_sentence * support_sentence).sum(1)  # 4032,
+        fake = (query_sentence * U_si).sum(1)  # 4032,
+        zero = torch.zeros(1).to(self.device)  # 1,
 
-        loss = torch.max(0.1 - real + fake, zero) # 4032,
+        loss = torch.max(0.1 - real + fake, zero)  # 4032,
         return loss
 
-    def meta_train(self, task_loader, num_batch_tasks=64, num_load_tasks=4, epoch=1000, writer=None, train=True):
+    def meta_train(
+            self,
+            task_loader,
+            num_batch_tasks=64,
+            num_load_tasks=4,
+            epoch=1000,
+            writer=None,
+            train=True):
         device = self.device
 
         B = num_batch_tasks
@@ -62,71 +76,103 @@ class TecNets(MetaLearner):
                 loss_emb, loss_ctr_U, loss_ctr_q = 0, 0, 0
                 support_sentence_list, query_sentence_list = [], []
 
-            support_image = tasks["support_images"] # N,support_num,100,3,125,125
+            # N,support_num,100,3,125,125
+            support_image = tasks["support_images"]
             support_state = tasks["support_states"]   # N,support_num,100,20
-            support_action = tasks["support_actions"] # N,support_num,100,7
-            support_language = tasks['support_languages']  # len(support), 1, 128.
+            support_action = tasks["support_actions"]  # N,support_num,100,7
+            # len(support), 1, 128.
+            support_language = tasks['support_languages']
 
             query_image = tasks["query_images"]  # N,query_num,100,3,125,125
             query_state = tasks["query_states"]    # N,query_num,100,20
             query_action = tasks["query_actions"]  # N,query_num,100,7
             query_language = tasks['query_languages']  # # len(query), 1, 128.
-            
-            support_num, query_num = len(support_image[1]), len(query_image[1])
-            size = support_image.shape[4] # 125 or 64
 
-            support_sentence = self.make_sentence(support_image, normalize=True)  # N,20
-            query_sentence = self.make_sentence(query_image, normalize=True) # N,20
-            
+            support_num, query_num = len(support_image[1]), len(query_image[1])
+            size = support_image.shape[4]  # 125 or 64
+
+            support_sentence = self.make_sentence(
+                support_image, normalize=True)  # N,20
+            query_sentence = self.make_sentence(
+                query_image, normalize=True)  # N,20
+
             support_sentence_list.append(support_sentence)
             query_sentence_list.append(query_sentence)
 
             # ---- calc loss_ctr ----
-            support_image = support_image.view(N * support_num * 100,3,size,size).to(device) # N * support_num * 100,C,H,W
-            support_state = support_state.view(N * support_num * 100,20).to(device)            # N * support_num * 100,20
-            support_action = support_action.view(N * support_num * 100,7).to(device)           # N * support_num * 100,7
-            
-            query_image = query_image.view(N * query_num * 100,3,size,size).to(device) # N * query_num * 100,C,H,W
-            query_state = query_state.view(N * query_num * 100,20).to(device)            # N * query_num * 100,20
-            query_action = query_action.view(N * query_num * 100,7).to(device)           # N * query_num * 100,7
-            support_sentence_U_inp = support_sentence.repeat_interleave(100 * support_num, dim=0)        # N * support_num * 100,20
-            support_sentence_q_inp = support_sentence.repeat_interleave(100 * query_num, dim=0)        # N * query_num * 100,20
+            support_image = support_image.view(
+                N * support_num * 100, 3, size, size).to(device)  # N * support_num * 100,C,H,W
+            support_state = support_state.view(
+                N * support_num * 100,
+                20).to(device)            # N * support_num * 100,20
+            support_action = support_action.view(
+                N * support_num * 100,
+                7).to(device)           # N * support_num * 100,7
 
-            U_out = self.ctr_net(support_image, support_sentence_U_inp, support_state)
-            _loss_ctr_U = self.loss_fn(U_out, support_action)  *  len(support_image)  *  0.1
-            
+            query_image = query_image.view(
+                N * query_num * 100, 3, size, size).to(device)  # N * query_num * 100,C,H,W
+            query_state = query_state.view(
+                N * query_num * 100,
+                20).to(device)            # N * query_num * 100,20
+            query_action = query_action.view(
+                N * query_num * 100,
+                7).to(device)           # N * query_num * 100,7
+            support_sentence_U_inp = support_sentence.repeat_interleave(
+                100 * support_num, dim=0)        # N * support_num * 100,20
+            support_sentence_q_inp = support_sentence.repeat_interleave(
+                100 * query_num, dim=0)        # N * query_num * 100,20
+
+            U_out = self.ctr_net(
+                support_image,
+                support_sentence_U_inp,
+                support_state)
+            _loss_ctr_U = self.loss_fn(
+                U_out, support_action) * len(support_image) * 0.1
+
             # import ipdb; ipdb.set_trace()
-            
+
             if train:
-                _loss_ctr_U.backward(retain_graph=True) # memory saving
+                _loss_ctr_U.backward(retain_graph=True)  # memory saving
             loss_ctr_U += _loss_ctr_U.item()
 
-            q_out = self.ctr_net(query_image, support_sentence_q_inp, query_state)
-            _loss_ctr_q = self.loss_fn(q_out, query_action) * len(query_image)  *  0.1
-            
+            q_out = self.ctr_net(
+                query_image,
+                support_sentence_q_inp,
+                query_state)
+            _loss_ctr_q = self.loss_fn(
+                q_out, query_action) * len(query_image) * 0.1
 
             if train:
-                _loss_ctr_q.backward(retain_graph=True) # memory saving
+                _loss_ctr_q.backward(retain_graph=True)  # memory saving
             loss_ctr_q += _loss_ctr_q.item()
             # ----
 
-            if ((i+1) * N) % B == 0:
+            if ((i + 1) * N) % B == 0:
 
-                # don't convert into list. graph informations will be lost. (and an error will occur)
-                support_sentence_list = torch.cat(support_sentence_list, 0) # N * (B/N),20 -> N,20
-                query_sentence_list = torch.cat(query_sentence_list, 0) # N * (B/N),20 -> N,20
+                # don't convert into list. graph informations will be lost.
+                # (and an error will occur)
+                support_sentence_list = torch.cat(
+                    support_sentence_list, 0)  # N * (B/N),20 -> N,20
+                query_sentence_list = torch.cat(
+                    query_sentence_list, 0)  # N * (B/N),20 -> N,20
 
                 query_sentence_j_list, support_sentence_j_list, U_si_list = [], [], []
 
                 # ---- calc loss_emb ----
-                for jdx, (query_sentence_j, support_sentence_j) in enumerate(zip(query_sentence_list, support_sentence_list)):
+                for jdx, (query_sentence_j, support_sentence_j) in enumerate(
+                        zip(query_sentence_list, support_sentence_list)):
                     for idx, U_si in enumerate(support_sentence_list):
-                        if jdx == idx: continue
+                        if jdx == idx:
+                            continue
                         query_sentence_j_list.append(query_sentence_j)
                         support_sentence_j_list.append(support_sentence_j)
                         U_si_list.append(U_si)
 
-                _loss_emb = torch.sum(self.cos_hinge_loss(query_sentence_j_list, support_sentence_j_list, U_si_list))  *  1.0
+                _loss_emb = torch.sum(
+                    self.cos_hinge_loss(
+                        query_sentence_j_list,
+                        support_sentence_j_list,
+                        U_si_list)) * 1.0
 
                 if train:
                     _loss_emb.backward()
@@ -149,7 +195,15 @@ class TecNets(MetaLearner):
         loss_ctr_q = np.mean(loss_ctr_q_list)
         loss = np.mean(loss_list)
 
-        print("loss:", loss, ", loss_ctr_U:", loss_ctr_U, ", loss_ctr_q:", loss_ctr_q, ", loss_emb:", loss_emb)
+        print(
+            "loss:",
+            loss,
+            ", loss_ctr_U:",
+            loss_ctr_U,
+            ", loss_ctr_q:",
+            loss_ctr_q,
+            ", loss_emb:",
+            loss_emb)
 
         # if writer:
         #     writer.add_scalar('loss_emb', loss_emb, epoch)
@@ -167,18 +221,30 @@ class TecNets(MetaLearner):
         with torch.no_grad():
             self.emb_net.eval()
             self.ctr_net.eval()
-            self.meta_train(task_loader, num_batch_tasks, num_load_tasks, epoch, False)
+            self.meta_train(
+                task_loader,
+                num_batch_tasks,
+                num_load_tasks,
+                epoch,
+                False)
 
     def make_test_sentence(self, demo_path, emb_net):
         inp = vread(demo_path)
-        cv2.imshow("demo", inp[-1][:,:,::-1]); cv2.waitKey(10)
-        inp = torch.stack([torch.from_numpy(inp).to("cuda")]) # 1,F,H,W,C
-        inp = (inp.permute(0,1,4,2,3).to(torch.float32)-127.5)/127.5 # 1,F,C,H,W
-        inp = self.make_sentence(inp, normalize=True) # 20,
-        return  inp
+        cv2.imshow("demo", inp[-1][:, :, ::-1])
+        cv2.waitKey(10)
+        inp = torch.stack([torch.from_numpy(inp).to("cuda")])  # 1,F,H,W,C
+        inp = (
+            inp.permute(
+                0, 1, 4, 2, 3).to(
+                torch.float32) - 127.5) / 127.5  # 1,F,C,H,W
+        inp = self.make_sentence(inp, normalize=True)  # 20,
+        return inp
 
     def sim_test(self, env, demo_path):
         with torch.no_grad():
             self.emb_net.eval()
             self.ctr_net.eval()
-            return super(TecNets, self).sim_test(env, demo_path, self.make_test_sentence)
+            return super(TecNets,self).sim_test(
+                env,
+                demo_path,
+                self.make_test_sentence)
--- tecnetsdataset.py
+++ tecnetsdataset.py
@@ -4,45 +4,47 @@ import numpy as np
 from torch.utils.data import Dataset
 from skimage import data, io
 from PIL import Image
-import pickle 
+import pickle
+
 
 class Tecnetsdataset(Dataset):
     def __init__(self, demo_dir='./datasets/mil_sim_push/', train=True):
-        # select the gif folder. 
+        # select the gif folder.
         if train:
             task_paths = f'{demo_dir}train/task_*.pkl'
         else:
-            task_paths = f'{demo_dir}test/task_*.pkl'     
-        self.task_info_paths = glob.glob(task_paths) 
+            task_paths = f'{demo_dir}test/task_*.pkl'
+        self.task_info_paths = glob.glob(task_paths)
 
     def __len__(self):
         return len(self.task_info_paths)
 
     def __getitem__(self, index, num_support=5, num_query=1):
         # get data from task_paths.
-        pickle_file = self.task_info_paths[index]     
+        pickle_file = self.task_info_paths[index]
         with open(pickle_file, 'rb') as f:
             data = pickle.load(f)
 
         # import pdb; pdb.set_trace()
         # sample query, support from 6 ~ 18.
         num_sample = num_support + num_query
-        support_query_sample_index = np.random.choice(12, num_sample, replace=False)
+        support_query_sample_index = np.random.choice(
+            12, num_sample, replace=False)
 
         # print('support_query_sample_index : ', support_query_sample_index)
         # test_sample_index = np.random.choice(list(set(range(12)) - set(support_query_sample_index)), num_sample, replace=False)
         # print('test_sample_index : ', test_sample_index)
 
-        actions, states = [], [] # len(query + support), xx
+        actions, states = [], []  # len(query + support), xx
         images = []
-        for sample_index in support_query_sample_index:    
+        for sample_index in support_query_sample_index:
             demo_path = self.task_info_paths[index][:-4] + f'/cond{sample_index + 6}*/*'
-            demo_paths = glob.glob(demo_path) 
-            
+            demo_paths = glob.glob(demo_path)
+
             image = self._get_gif(demo_paths)
             # print(type(image))
             image = torch.stack(image)  # list to tensors.
-            images.append(image) # list of tensors
+            images.append(image)  # list of tensors
 
             action = data['actions'][sample_index]
             actions.append(torch.from_numpy(action.astype(np.float32)).clone())
@@ -50,14 +52,14 @@ class Tecnetsdataset(Dataset):
             state = data['states'][sample_index]
             states.append(torch.from_numpy(state.astype(np.float32)).clone())
 
-        language_path = './datasets/2021_instructions/' + data['demo_selection'].split('/')[-1][:-4] + '.npy'
+        language_path = './datasets/2021_instructions/' + \
+            data['demo_selection'].split('/')[-1][:-4] + '.npy'
         language = np.load(language_path)
 
         # print(language.shape)
         # print(np.array(actions).shape)
         # print(np.array(images).shape)
         # print(np.array(states).shape)
-
 
         # import ipdb; ipdb.set_trace()
         # actions = np.array(actions)
@@ -69,25 +71,31 @@ class Tecnetsdataset(Dataset):
         states = torch.stack(states)
         # print('states_shape : ',states.shape)  # torch.Size([6, 100, 20])
         images = torch.stack(images)
-        # print('images_shape : ', images.shape)  # torch.Size([6, 100, 125, 125, 3])
+        # print('images_shape : ', images.shape)  # torch.Size([6, 100, 125,
+        # 125, 3])
         language = torch.from_numpy(language.astype(np.float32)).clone()
         # print('langauge_shape : ', language.shape)  # torch.Size([1, 128])
-        support_actions, query_actions = actions.split([num_support, num_query], dim=0)
-        support_states, query_states = states.split([num_support, num_query], dim=0)
-        support_images, query_images = images.split([num_support, num_query], dim=0)
+        support_actions, query_actions = actions.split(
+            [num_support, num_query], dim=0)
+        support_states, query_states = states.split(
+            [num_support, num_query], dim=0)
+        support_images, query_images = images.split(
+            [num_support, num_query], dim=0)
 
         task_info = {
             'support_actions': support_actions,  # len(support), 100, 7.
-            'support_states': support_states, # len(support), 100,20.
-            'support_images': ((support_images.permute(0,1,4,2,3).to(torch.float32) - 127.5)/127.5),  # len(support), 100, 3, 125, 125.
-            'support_languages': language, # len(support), 1, 128.
+            'support_states': support_states,  # len(support), 100,20.
+            # len(support), 100, 3, 125, 125.
+            'support_images': ((support_images.permute(0, 1, 4, 2, 3).to(torch.float32) - 127.5) / 127.5),
+            'support_languages': language,  # len(support), 1, 128.
             'query_actions': query_actions,  # len(query), 100, 7.
-            'query_states': query_states, # len(query), 100, 20.
-            'query_images': ((query_images.permute(0,1,4,2,3).to(torch.float32) - 127.5)/127.5),  # len(query), 100, 3, 125, 125.
-            'query_languages': language, # len(query), 1, 128.
+            'query_states': query_states,  # len(query), 100, 20.
+            # len(query), 100, 3, 125, 125.
+            'query_images': ((query_images.permute(0, 1, 4, 2, 3).to(torch.float32) - 127.5) / 127.5),
+            'query_languages': language,  # len(query), 1, 128.
         }
         return task_info
-    
+
     def _get_gif(self, demo_paths):
         image = []
         for demo in demo_paths:
@@ -96,4 +104,3 @@ class Tecnetsdataset(Dataset):
             x = torch.from_numpy(x.astype(np.float32)).clone()
             image.append(x)
         return image
-        
