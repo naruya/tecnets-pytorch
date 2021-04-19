2cbca84320ed3544a4cba95639fb3ef7a92e18d2
shogi880 <shogi-880@g.ecc.u-tokyo.ac.jp>
2021-04-19 13:53:36 +0900

    comennt out

--- scripts/prepare_data.py
+++ scripts/prepare_data.py
@@ -50,50 +50,50 @@ def test():
                 x = Image.open(demo)
                 x = x.convert('RGB')
                 x.save(demo[:-4] + ".png")
-                x = torch.from_numpy(np.array(x))
-                image.append(x)
-            # print(type(image))
-            image = torch.stack(image)  # list to tensors.
-            images.append(image)  # list of tensors
+        #         x = torch.from_numpy(np.array(x))
+        #         image.append(x)
+        #     # print(type(image))
+        #     image = torch.stack(image)  # list to tensors.
+        #     images.append(image)  # list of tensors
 
-            action = data['actions'][sample_index]
-            actions.append(torch.from_numpy(action.astype(np.float32)).clone())
+        #     action = data['actions'][sample_index]
+        #     actions.append(torch.from_numpy(action.astype(np.float32)).clone())
 
-            state = data['states'][sample_index]
-            states.append(torch.from_numpy(state.astype(np.float32)).clone())
+        #     state = data['states'][sample_index]
+        #     states.append(torch.from_numpy(state.astype(np.float32)).clone())
 
-        language_path = './datasets/2021_instructions/' + \
-            data['demo_selection'].split('/')[-1][:-4] + '.npy'
-        language = np.load(language_path)
+        # language_path = './datasets/2021_instructions/' + \
+        #     data['demo_selection'].split('/')[-1][:-4] + '.npy'
+        # language = np.load(language_path)
 
-        actions = torch.stack((actions))
-        # print('actions_shape : ',actions.shape)  # torch.Size([6, 100, 7])
-        states = torch.stack(states)
-        # print('states_shape : ',states.shape)  # torch.Size([6, 100, 20])
-        images = torch.stack(images)
-        # print('images_shape : ', images.shape)  # torch.Size([6, 100, 125,
-        # 125, 3])
-        language = torch.from_numpy(language.astype(np.float32)).clone()
-        # print('langauge_shape : ', language.shape)  # torch.Size([1, 128])
-        support_actions, query_actions = actions.split(
-            [num_support, num_query], dim=0)
-        support_states, query_states = states.split(
-            [num_support, num_query], dim=0)
-        support_images, query_images = images.split(
-            [num_support, num_query], dim=0)
+        # actions = torch.stack((actions))
+        # # print('actions_shape : ',actions.shape)  # torch.Size([6, 100, 7])
+        # states = torch.stack(states)
+        # # print('states_shape : ',states.shape)  # torch.Size([6, 100, 20])
+        # images = torch.stack(images)
+        # # print('images_shape : ', images.shape)  # torch.Size([6, 100, 125,
+        # # 125, 3])
+        # language = torch.from_numpy(language.astype(np.float32)).clone()
+        # # print('langauge_shape : ', language.shape)  # torch.Size([1, 128])
+        # support_actions, query_actions = actions.split(
+        #     [num_support, num_query], dim=0)
+        # support_states, query_states = states.split(
+        #     [num_support, num_query], dim=0)
+        # support_images, query_images = images.split(
+        #     [num_support, num_query], dim=0)
 
-        task_info = {
-                'support_actions': support_actions,  # len(support), 100, 7.
-                'support_states': support_states,  # len(support), 100,20.
-                # len(support), 100, 3, 125, 125.
-                'support_images': ((support_images.permute(0, 1, 4, 2, 3).to(torch.float32) - 127.5) / 127.5),
-                'support_languages': language,  # len(support), 1, 128.
-                'query_actions': query_actions,  # len(query), 100, 7.
-                'query_states': query_states,  # len(query), 100, 20.
-                # len(query), 100, 3, 125, 125.
-                'query_images': ((query_images.permute(0, 1, 4, 2, 3).to(torch.float32) - 127.5) / 127.5),
-                'query_languages': language,  # len(query), 1, 128.
-            }
+        # task_info = {
+        #         'support_actions': support_actions,  # len(support), 100, 7.
+        #         'support_states': support_states,  # len(support), 100,20.
+        #         # len(support), 100, 3, 125, 125.
+        #         'support_images': ((support_images.permute(0, 1, 4, 2, 3).to(torch.float32) - 127.5) / 127.5),
+        #         'support_languages': language,  # len(support), 1, 128.
+        #         'query_actions': query_actions,  # len(query), 100, 7.
+        #         'query_states': query_states,  # len(query), 100, 20.
+        #         # len(query), 100, 3, 125, 125.
+        #         'query_images': ((query_images.permute(0, 1, 4, 2, 3).to(torch.float32) - 127.5) / 127.5),
+        #         'query_languages': language,  # len(query), 1, 128.
+        #     }
 
         # print(task_info)
 test()
