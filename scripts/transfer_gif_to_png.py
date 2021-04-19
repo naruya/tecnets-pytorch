348c603010a4795f874ba567efeb370b29784ae6
shogi880 <shogi-880@g.ecc.u-tokyo.ac.jp>
2021-04-19 07:11:22 +0000

    transfer gif to jpg multiprocess

--- scripts/transfer_gif_to_png.py
+++ scripts/transfer_gif_to_png.py
@@ -6,21 +6,19 @@ import multiprocessing as mp
 import os
 
 demo_dir = '/root/datasets/mil_sim_push/'
-task_paths = f'{demo_dir}train/task_*.pkl'
+task_paths = f'{demo_dir}new_test/task_*.pkl'
 task_info_paths = glob.glob(task_paths)
-
+print(len(task_info_paths))
 
 def test(name, param):
     for index in param:
-        for sample_index in range(12):
-            if index >= len(task_info_paths): continue
-            demo_path = task_info_paths[index][:-4] + f'/cond{sample_index + 6}*/*.gif'
-            demo_paths = glob.glob(demo_path)
-            # _get_gif
-#            print(demo_paths[0])
-            for demo in demo_paths:
-                if os.path.exists(demo[:-4] + ".jpg"):
-                    Image.open(demo).convert('RGB').save(demo[:-4] + ".jpg")
+        if index >= len(task_info_paths): continue
+        demo_path = task_info_paths[index][:-4] + f'/cond*/*.gif'
+        demo_paths = glob.glob(demo_path)
+        print(index)
+        for demo in demo_paths:
+            if os.path.exists(demo[:-4] + ".jpg"): continue
+            Image.open(demo).convert('RGB').save(demo[:-4] + ".jpg")
 
 
 if __name__ == '__main__':
@@ -28,23 +26,9 @@ if __name__ == '__main__':
     num_cores = int(mp.cpu_count())
     print("Local multi cpu : " + str(num_cores) + "cores")
     pool = mp.Pool(num_cores)
-    param_dict = {'task1': list(range(0, 50)),
-                  'task2': list(range(50, 100)),
-                  'task3': list(range(100, 150)),
-                  'task4': list(range(150, 200)),
-                  'task5': list(range(200, 250)),
-                  'task6': list(range(250, 300)),
-                  'task8': list(range(300, 350)),
-                  'task9': list(range(350, 400)),
-                  'task10': list(range(400, 450)),
-                  'task11': list(range(450, 500)),
-                  'task12': list(range(500, 550)),
-                  'task13': list(range(400, 500)),
-                  'task14': list(range(500, 550)),
-                  'task15': list(range(550, 600)),
-                  'task16': list(range(600, 650)),
-                  'task17': list(range(650, 700)),
-                  'task18': list(range(700, 800))}
+    param_dict = {}
+    for i in range(40):
+        param_dict[f"task{i}"] = list(range(i*2, (i+1)*2))
     results = [pool.apply_async(test, args=(name, param)) for name, param in param_dict.items()]
     results = [p.get() for p in results]    
     
