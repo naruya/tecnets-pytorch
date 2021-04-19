6fd8764b20917009e153af4fb43636087a0dfb87
shogi880 <shogi-880@g.ecc.u-tokyo.ac.jp>
2021-04-19 17:27:26 +0900

    fix

--- scripts/prepare_data.py
+++ scripts/prepare_data.py
@@ -18,49 +18,41 @@ print("task_info_paths: ", len(task_info_paths), task_info_paths[0])
 def test(name, param):
     for index in param:
         if index >= len(task_info_paths): continue
-        # print(index)
-#        pickle_file = task_info_paths[index]
-#        print(pickle_file.split('/')[-1][:9])
-#        if pickle_file.split('/')[-1][:9] == "task_info": os.remove(pickle_file)
-#        if len(pickle_file) > len("/root/datasets/mil_sim_push/train/task_info_4.pkl"): 
-#            print(pickle_file)
-#            os.remove(pickle_file)
-#            file = pathlib.Path(pickle_file)
-#            file.unlink()
-#         with open(pickle_file, 'rb') as f:
-#             data = pickle.load(f)
-#         if index == 0: print(data)
-#         path_ = pickle_file.split('/')
-#         output_name = '/'.join(path_[:-1]) + '/taskinfo' + path_[-1][4:]
+        pickle_file = task_info_paths[index]
+        with open(pickle_file, 'rb') as f:
+            data = pickle.load(f)
+        # if index == 0: print(data)
+        path_ = pickle_file.split('/')
+        output_name = '/'.join(path_[:-1]) + '/taskinfo' + path_[-1][4:]
 
-#         action = data['actions']
-#         # actions.append(action)
-# #        actions = []
-# #        actions.append(torch.from_numpy(action.astype(np.float32)))
-# #        actions = torch.stack(actions)
-#         # print(type(actions), actions.shape)
+        action = data['actions']
+        # actions.append(action)
+#        actions = []
+#        actions.append(torch.from_numpy(action.astype(np.float32)))
+#        actions = torch.stack(actions)
+        # print(type(actions), actions.shape)
 
-#         state = data['states']
-# #        states = []
-# #        states.append(torch.from_numpy(state.astype(np.float32)))
-# #        states = torch.stack(states)
-#         # print(type(states), states.shape)
+        state = data['states']
+#        states = []
+#        states.append(torch.from_numpy(state.astype(np.float32)))
+#        states = torch.stack(states)
+        # print(type(states), states.shape)
 
-#         language_path = './datasets/2021_instructions/' + \
-#             data['demo_selection'].split('/')[-1][:-4] + '.npy'
-#         language = np.load(language_path)
-#         language = torch.from_numpy(language.astype(np.float32))
+        language_path = './datasets/2021_instructions/' + \
+            data['demo_selection'].split('/')[-1][:-4] + '.npy'
+        language = np.load(language_path)
+        language = torch.from_numpy(language.astype(np.float32))
 
-#         # print(type(language), language.shape)
-#         task_info = {
-#             'demo_selection': data['demo_selection'],
-#             'states': state,
-#             'actions': action,
-#             'instructions': language
-#         }
-#         with open(output_name, 'wb') as f:
-#             pickle.dump(task_info, f)
-#             print(output_name)
+        # print(type(language), language.shape)
+        task_info = {
+            'demo_selection': data['demo_selection'],
+            'states': state,
+            'actions': action,
+            'instructions': language
+        }
+        with open(output_name, 'wb') as f:
+            pickle.dump(task_info, f)
+            print(output_name)
 
 
 if __name__ == '__main__':
