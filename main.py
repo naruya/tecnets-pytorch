da06c77a97c6058cc39bacab799f0eccdabebf13
shogi880 <shogi-880@g.ecc.u-tokyo.ac.jp>
2021-04-20 17:07:01 +0900

    debug

--- main.py
+++ main.py
@@ -13,8 +13,8 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Task-Embedded Control Networks Implementation')
     parser.add_argument('--device_ids', type=int, nargs='+', help='list of CUDA devices (default: [0])', default=[0])
     parser.add_argument('--demo_dir', type=str, default='/root/datasets/mil_sim_push/')
-    parser.add_argument('--num_batch_tasks', type=int, default=32)
-    parser.add_argument('--num_load_tasks', type=int, default=32)
+    parser.add_argument('--num_batch_tasks', type=int, default=128)
+    parser.add_argument('--num_load_tasks', type=int, default=128)
     parser.add_argument('--train_n_shot', type=int, default=1)
     parser.add_argument('--test_n_shot', type=int, default=1)
     parser.add_argument('--learning_rate', type=float, default=0.0005)
@@ -50,13 +50,12 @@ if __name__ == '__main__':
                                    num_workers=os.cpu_count(),
                                    pin_memory=True,
                                    drop_last=True)
-    valid_task_loader = DataLoader(TecnetsDataset(demo_dir=args.demo_dir),
-                                   batch_size=args.num_load_tasks,
-                                   shuffle=True,
-                                   num_workers=os.cpu_count(),
-                                   pin_memory=True,
-                                   drop_last=True)
-    
+    # valid_task_loader = DataLoader(TecnetsDataset(demo_dir=args.demo_dir),
+    #                                batch_size=args.num_load_tasks,
+    #                                shuffle=True,
+    #                                num_workers=os.cpu_count(),
+    #                                pin_memory=True,
+    #                                drop_last=True)
     torch.backends.cudnn.benchmark = True
 
     meta_epochs = 2  # 400000/11
@@ -67,4 +66,4 @@ if __name__ == '__main__':
         meta_learner.meta_train(task_loader=train_task_loader, num_batch_tasks=args.num_batch_tasks, num_load_tasks=args.num_load_tasks, epoch=epoch)
         # print(prof.table())
         # prof.export_chrome_trace('./logs/profile.json')
-        meta_learner.meta_valid(task_loader=valid_task_loader, num_batch_tasks=args.num_batch_tasks, num_load_tasks=args.num_load_tasks, epoch=epoch)
+        meta_learner.meta_valid(task_loader=train_task_loader, num_batch_tasks=args.num_batch_tasks, num_load_tasks=args.num_load_tasks, epoch=epoch)
